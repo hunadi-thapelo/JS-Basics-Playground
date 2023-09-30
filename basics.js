a5f57b638325f9818b584b8b189c826bd4f781ce
hunadim <hmapulane@gmail.com>
2023-09-30 12:37:33 +0200

    working with let keyword

---
+++ basics.js
@@ -0,0 +1,30 @@
+console.log("Hello World!")
+ //to print to console type node <filename.js> in terminal
+
+let a = 10
+
+console.log(a)
+console.log(typeof(a)) //typeof gives you data type of varaible
+
+
+/*String*/
+let b = 'QA Engineer'
+console.log(typeof(b))
+
+
+/*boolen*/
+let isRequired = true
+console.log(typeof(isRequired))
+
+let c = 123.5
+console.log(typeof(c))
+
+//let c = a + b
+
+//ES6 version --> let, const
+/*
+Difference between let, const
+
+let is a signal that the identifier may be reassigned    
+const is a signal that the identifier won't be reassigned
+*/

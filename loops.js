eb65790c7438e087c2a9c444bde0c62fff4d1e83
hunadim <hmapulane@gmail.com>
2023-10-01 19:26:38 +0200

    handling basic if statement

---
+++ loops.js
@@ -0,0 +1,10 @@
+const flag = true
+
+if(!flag)//expression is reversed (with negation), however variable remains unchanged
+{
+    console.log("Condition is satisfied")
+}
+else
+{
+    console.log("condition is not satisfied")
+}

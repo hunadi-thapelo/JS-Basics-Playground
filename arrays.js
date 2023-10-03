e52941c590af5e4570ad92bf6b7839c505db11f8
hunadim <hmapulane@gmail.com>
2023-10-03 07:49:49 +0200

    adding a new value at end of array

--- arrays.js
+++ arrays.js
@@ -6,5 +6,5 @@ var marks = [55,67,90,88,78,93]
 
 console.log(marks.length) //get length or number of elements in array
 marks[3] = 90 //change an value in array
-
+marks.push(65) // add a new value at end of array
 console.log(marks)

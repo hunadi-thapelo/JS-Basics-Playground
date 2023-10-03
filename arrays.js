74319de6536c99d8f8f1a66f1b3afd0b50bda073
hunadim <hmapulane@gmail.com>
2023-10-03 07:47:37 +0200

    change value in an array

--- arrays.js
+++ arrays.js
@@ -5,5 +5,6 @@ var marks = new Array(55,67,90,88,78,93)
 var marks = [55,67,90,88,78,93]
 
 console.log(marks.length) //get length or number of elements in array
+marks[3] = 90 //change an value in array
 
 console.log(marks)

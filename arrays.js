2c05078a246f42cc156d1387d9e56c9c723da408
hunadim <hmapulane@gmail.com>
2023-10-03 07:46:31 +0200

    working with arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var marks = Array(6)
+var marks = new Array(55,67,90,88,78,93)
+
+//preferred and simpliest vway to declare an array
+var marks = [55,67,90,88,78,93]
+
+console.log(marks.length) //get length or number of elements in array
+
+console.log(marks)

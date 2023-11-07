971334d033fca3ea72e1b81c92c0e6b8e862083b
hunadim <hmapulane@gmail.com>
2023-11-07 08:55:31 +0200

    added var variable on global level with IF statement

--- functions-continued.js
+++ functions-continued.js
@@ -2,6 +2,12 @@
 //scope of var is global if declared in global level
 var greet = "Good Morning"
 
+//this greet scope is on global level, if statement is outside the greetings function
+//there greet var value will be replaced with good day
+if (1 == 1){
+    var greet = "Good day"
+}
+
 
 //or in function level only when declared in a function
 function greetings(a,b){

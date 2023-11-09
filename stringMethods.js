92d3de3804d1a3c8efd4a7569e74271b749f11ec
hunadim <hmapulane@gmail.com>
2023-11-09 09:01:03 +0200

    Handling basic string methods length and slice

---
+++ stringMethods.js
@@ -0,0 +1,9 @@
+let sport = "Basketball"
+//day = "Congratulations"
+
+//Get length of string
+console.log(sport.length)
+
+//Get length of string
+let subSport = sport.slice(0,6) //starts count from 0...
+console.log(subSport)

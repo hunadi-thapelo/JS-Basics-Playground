e87c8a829647ad6c65a4a064a988203597712d78
hunadim <hmapulane@gmail.com>
2023-11-09 09:34:02 +0200

    Hnalding Split method to split string into an array

--- stringMethods.js
+++ stringMethods.js
@@ -6,7 +6,13 @@ console.log(sport.length)
 
 //Get length of string
 let subSport = sport.slice(0,6) //starts count from 0...
-console.log(subSport)
+console.log(subSport) // Basket
 
 //access character in string
-console.log(sport[0])
+console.log(sport[0]) //B
+
+/* Split method to split string into an array */
+// Basket   all
+let sportSplit = sport.split("b")
+console.log(sportSplit[0]) // Baseket
+console.log(sportSplit[1]) // all

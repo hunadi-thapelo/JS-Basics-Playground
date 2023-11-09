83eaa0f0a17e8bc87382a1b7b0f3abde0c0fb865
hunadim <hmapulane@gmail.com>
2023-11-09 09:29:05 +0200

    Access character in string at index

--- stringMethods.js
+++ stringMethods.js
@@ -7,3 +7,6 @@ console.log(sport.length)
 //Get length of string
 let subSport = sport.slice(0,6) //starts count from 0...
 console.log(subSport)
+
+//access character in string
+console.log(sport[0])

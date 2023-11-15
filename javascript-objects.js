14dc7bcac5162fc805fa23d0e2d5a4ae1f82ae5b
hunadim <hmapulane@gmail.com>
2023-11-15 10:26:09 +0200

    Adding a new property to existing object

--- javascript-objects.js
+++ javascript-objects.js
@@ -11,3 +11,7 @@ console.log(person["lastName"])
 //Change a property value
 person.firstName = "Rebathile"
 console.log(person.firstName)
+
+//Add a property to existing object
+person.gender = "female"
+console.log(person)

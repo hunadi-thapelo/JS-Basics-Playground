d290ee018d0502c29d824ce718a98003059c52ad
hunadim <hmapulane@gmail.com>
2023-11-19 18:32:27 +0200

    adds get city method

--- javascript-classes.js
+++ javascript-classes.js
@@ -3,8 +3,18 @@ class Person{
     //define property of class
     age = 30
 
+    /*getter method if prop holds multiple lines of code
+      get is just a property and not a method
+    */
+    get city(){
+        return 'Johannesburg'
+
+    }
+
 }
 
 //To access property - create object of class
 let personOne = new Person()
 console.log(personOne.age)
+
+console.log(personOne.city)

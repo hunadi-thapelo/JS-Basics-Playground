f1d0e55c04a1cfa03180e99b4bcc49d088d03a68
hunadim <hmapulane@gmail.com>
2025-03-04 10:43:05 +0200

    adds js string interpolation to combine string

--- basics.js
+++ basics.js
@@ -1,22 +1,42 @@
 console.log("Hello World!")
  //to print to console type node <filename.js> in terminal
 
-let a = 10
+let a = 10;
 
 console.log(a)
 console.log(typeof(a)) //typeof gives you data type of varaible
 
 
 /*String*/
-let b = 'QA Engineer'
-console.log(typeof(b))
+let b = 'QA Engineer';
+let firstName = 'Jane';
+let lastName = 'Smith';
+
+//Concatenation
+let fullName1 = firstName + ' ' + lastName;
+
+//Using JS string interpolation
+
+let fullname = `${firstName} ${lastName}`;
+
+console.log(typeof(b));
+
+console.log('Print fullname below using JS concatenation:')
+console.log(fullName1);
+
+console.log('Print fullname below using JS string interpolation:')
+console.log(fullname);
+
+
+
+
 
 
 /*boolen*/
-let isRequired = true
+let isRequired = true;
 console.log(typeof(isRequired))
 
-let c = 123.5
+let c = 123.5;
 console.log(typeof(c))
 
 //let c = a + b

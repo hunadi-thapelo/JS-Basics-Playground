d3589a5e2b476cae01db61305a79a96c81b7274e
hunadim <hmapulane@gmail.com>
2025-03-05 08:41:18 +0200

    swaps variables using destructing assignment

--- swap-two-variables.js
+++ swap-two-variables.js
@@ -10,3 +10,11 @@ firstName = lastName;
 lastName = temp;
 
 console.log(`${firstName} ${lastName}`)
+
+
+//using destructing assignment - can swap strings and other data types
+let a = "October";
+let b = 19;
+
+[a,b] = [b, a];
+console.log(a + " " + b);

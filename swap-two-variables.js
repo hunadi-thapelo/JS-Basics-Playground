572366bd2c33f441bbcc187b79a418931fed07cc
hunadim <hmapulane@gmail.com>
2025-03-05 08:02:04 +0200

    swaps variables using a temp variable

---
+++ swap-two-variables.js
@@ -0,0 +1,12 @@
+
+//using a temp variable
+let firstName = 'Dlamini';
+let lastName = 'Naledi';
+let temp;
+
+//swap variables
+temp = firstName;
+firstName = lastName;
+lastName = temp;
+
+console.log(`${firstName} ${lastName}`)

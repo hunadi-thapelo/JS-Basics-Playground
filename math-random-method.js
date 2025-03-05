abfbd2b56ab32240065dbacd59393d1f73c5fb0d
hunadim <hmapulane@gmail.com>
2025-03-05 07:14:29 +0200

    adds function to generate random number

--- math-random-method.js
+++ math-random-method.js
@@ -5,6 +5,14 @@ function getRandomEmail(domain){
     return randomText = Math.random().toString(36).substring(7) + '@' + domain
 }
 
+//generate random number and returns only whole number
+function randomNumber(number){
+
+    return Math.floor((Math.random(number) * 100));
+}
+
 //print to console
 //console.log(getRandomEmail('google.com'));
 getRandomEmail('google.com');
+console.log(randomNumber(20));
+

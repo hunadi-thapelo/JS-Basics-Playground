51d69b169475c5d9d29d6edb1c9718de968c5df8
hunadim <hmapulane@gmail.com>
2026-02-10 08:36:19 +0200

    JS conditions with while loops

---
+++ condition-while-loop.js
@@ -0,0 +1,21 @@
+
+/* ----------while loop  ---------------*/
+let i = 0
+
+while(i<10)
+{
+    i++
+    console.log(i)
+}
+
+
+
+/* ---------- do-while loop  ---------------*/
+//Example: do...while loop can be used when 
+//requirement is to check for a status, check status of empty field validation when trying to log in
+console.log("*********** do while loop **********")
+let j=0
+do{
+    j++
+} while(j>10);
+console.log(j)
